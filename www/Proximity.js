26611343ddd4cb38c5eefae2484b8b7a5c56218b
Wes Reid <wesley.reid@gmail.com>
2014-05-23 01:21:40 -0400

    modify js

--- www/Proximity.js
+++ www/Proximity.js
@@ -1,34 +1,33 @@
-cordova.define("com.razorfish.proximityPlugin", function(require, exports, module) { var Proximity = function() {
+var Proximity = function() {
 };
 
-
+alert('entering proximityPlugin.js');
 // Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
-    Proximity.prototype.saySomething = function(successCallback, errorCallback, msg) {
-        if (errorCallback == null) { errorCallback = function() {}}
+Proximity.prototype.saySomething = function(successCallback, errorCallback, msg) {
+    if (errorCallback == null) { errorCallback = function() {}}
 
-        if (typeof errorCallback != "function")  {
-            console.log("Proximity.register failure: failure parameter not a function");
-            return
-        }
+    if (typeof errorCallback != "function")  {
+        console.log("Proximity.register failure: failure parameter not a function");
+        return
+    }
 
-        if (typeof successCallback != "function") {
-            console.log("Proximity.register failure: success callback parameter must be a function");
-            return
-        }
+    if (typeof successCallback != "function") {
+        console.log("Proximity.register failure: success callback parameter must be a function");
+        return
+    }
 
-        cordova.exec(successCallback, errorCallback, "proximityPlugin", "saySomething", [{"msg":msg}]);
-    };
+    cordova.exec(successCallback, errorCallback, "proximityPlugin", "saySomething", [{"msg":msg}]);
+};
 
 //-------------------------------------------------------------------
 
-    if(!window.plugins) {
-        window.plugins = {};
-    }
-    if (!window.plugins.proximity) {
-        window.plugins.proximity = new Proximity();
-    }
+if(!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.proximity) {
+    window.plugins.proximity = new Proximity();
+}
 
-    if (module.exports) {
-        module.exports = Proximity;
-    }
-});
+if (module.exports) {
+    module.exports = Proximity;
+}
